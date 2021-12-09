e870fd27f898a67238385a3618bb1b11ceccb47a
soranoh <sora2019@naver.com>
2021-12-09 15:02:59 +0900

    #2.4 Layout System

--- App.js
+++ App.js
@@ -1,30 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View } from 'react-native';
 
 /*
- * 1) StatusBar 는 시계, 배터리, Wi-Fi 등 운영체제의 상단 상태바와 소통하는 방법이다.
- * 2) StyleSheet.create 를 사용해야 자동완성 기능이 가능하다.
- * 3) 웹 브라우저가 아닌 앱!!! 이다.
+ * 1) 기본적으로 모든 View 는 Flex Container 다.
+ * 2) 웹에서 Flex Direction 의 기본값은 Row 이지만, 모바일에서 기본값은 Column 이다. (=> flexDirection: "row" 로 변경 가능)
+ * 3) 웹이 아니기 때문에 Overflow 가 있어도 자동 스크롤이 불가능하다.
+ * 4) 부모 View 에 flex 를 지정하면, 자식 View 에서 flex 로 크기 비율을 설정할 수 있다.
  */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>** SoraWeather **</Text>
-      <StatusBar style="light" />
+    <View style={{flex: 1, flexDirection: "row"}}>
+      <View style={{flex: 1, backgroundColor: "tomato"}}></View>
+      <View style={{flex: 1, backgroundColor: "teal"}}></View>
+      <View style={{flex: 1, backgroundColor: "orange"}}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'darkblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-    color: "skyblue"
-  }
-});
